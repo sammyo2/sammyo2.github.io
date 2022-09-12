25bbf000fd84bd9ed1eee13a4e6cd682aa078818
sammyo2 <sammy.n.otoo@gmail.com>
2022-09-12 10:59:45 -0400

    testing latest proposed updates for additional "featured projects" section

--- index.js
+++ index.js
@@ -31,3 +31,37 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1450);
+
+
+
+
+//functions for project windows
+
+function showabout(){
+    $("#about_container").css("display","inherit");
+    $("#about_container").addClass("animated slideInLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideInLeft");
+    },800);
+}
+function closeabout(){
+    $("#about_container").addClass("animated slideOutLeft");
+    setTimeout(function(){
+        $("#about_container").removeClass("animated slideOutLeft");
+        $("#about_container").css("display","none");
+    },800);
+}
+function showwork(){
+    $("#work_container").css("display","inherit");
+    $("#work_container").addClass("animated slideInRight");
+    setTimeout(function(){
+        $("#work_container").removeClass("animated slideInRight");
+    },800);
+}
+function closework(){
+    $("#work_container").addClass("animated slideOutRight");
+    setTimeout(function(){
+        $("#work_container").removeClass("animated slideOutRight");
+        $("#work_container").css("display","none");
+    },800);
+}
